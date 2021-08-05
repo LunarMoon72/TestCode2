e0326ed5689fb209ed2c7f533b7b936e1bf4c484
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-08-05 18:26:51 -0400

    Create Main.php
    Let's see if this works...

---
+++ src/LunarMoon72/TestCode2/Main.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LunarMoon72\TestCode2;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\Player;
+
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnabled(){
+		$this->getLogger()->info("Plugin is Enabled");
+
+	}
+    
+    public function onDisabled(){
+    	$this->getLogger()->info("Plugin is Disabled")
+
+    }
+    
+    public function onJoin(PlayerJoinEvent $event){
+    	$player = $event->getPlayer();
+    	$name = $player->getName();
+    	$inv = $player->getInventory();
+
+    	$this->getServer()->boradcastMessage("$name has Joined the server!");
+    	$item = Item::get(345, 0 , 1);
+    	$inv->setItem(0, $item);
+    }
+
+}
